cd8520519a4374c29129dce04a84d0124ea64395
Zion livingstone <zionlivingstone4@gmail.com>
2023-10-05 00:08:03 +0100

    x

---
+++ app/blog/[slug]/page.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { createClient } from "contentful";
+import Image from 'next/image';
+const client = createClient({
+    space: process.env.SPACE_ID,
+    accessToken: process.env.ACCESS_TOKEN,
+});
+
+const page = async (prop) => {
+    const {params}=prop
+    const {slug}=params
+    
+    const fetchBlogPost = async (x) => {
+        const queryOptions = {
+          content_type: "blog",
+          "fields.slug[match]": x,
+        };
+        const queryResult = await client.getEntries(queryOptions);
+        return queryResult.items[0];
+      };
+
+      const blogPost= await fetchBlogPost(slug)
+      const {title,content,image}=blogPost.fields
+
+  return (
+    <div>
+      <Image src={`https:${image.fields.file.url}`} width={500} height={500} alt={title} />
+        <div className="text-4xl text-center">{title}</div>
+          
+         <p>{content}</p>
+           
+        
+            
+           
+      
+        </div>
+  )
+}
+
+export default page
+
+export async function generateStaticParams() {
+  const queryOptions = {
+    content_type: "blog",
+    select: "fields.slug",
+  };
+  const articles = await client.getEntries(queryOptions)
+  return articles.items.map((article) => ({
+    slug: article.fields.slug,
+  }));
+}
---
+++ app/blog/page.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { createClient } from "contentful";
+import Link from 'next/link';
+import Image from 'next/image';
+const client =  createClient({
+  space: process.env.SPACE_ID,
+  accessToken: process.env.ACCESS_TOKEN,
+});
+const getBlogEntries = async () => {
+    const entries = await client.getEntries({ content_type: "blog" });
+    return entries;
+  };
+const page = async () => {
+    const blogEntries = await getBlogEntries();
+    
+  return (
+    <div className='w-screen flex justify-center items-center gap-12 flex-col'>
+        <div className="text-center text-5xl">Blog</div>
+        <div className=" flex flex-col gap-8 w-[50%]">
+        {blogEntries.items.map((post)=>{
+           
+            const {title,slug,image,content} =post.fields
+            
+           return(
+            <div className=" h-24 rounded bg-blue-300" key={slug}>
+                <Link href={`/blog/${slug}`}>
+                <div className="text-2xl">{title}</div>
+                </Link>
+               
+            </div>
+           )
+        })}
+        </div>
+    </div>
+  )
+}
+
+export default page
