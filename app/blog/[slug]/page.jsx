2b5324ca62ab8cc761d605729ed2b1384902a058
Zion livingstone <zionlivingstone4@gmail.com>
2023-10-06 21:28:59 +0100

    x

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -6,7 +6,7 @@ const client = createClient({
     accessToken: process.env.ACCESS_TOKEN,
 });
 
-const page = async (prop) => {
+const page =  (prop) => {
     const {params}=prop
     const {slug}=params
     
@@ -19,7 +19,7 @@ const page = async (prop) => {
         return queryResult.items[0];
       };
 
-      const blogPost= await fetchBlogPost(slug)
+      const blogPost=  fetchBlogPost(slug)
       const {title,content,image}=blogPost.fields
 
   return (
