af31bf870057ee015b6e088a3de10e2e866c24bc
Zion livingstone <zionlivingstone4@gmail.com>
2023-10-06 21:38:28 +0100

    x

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -1,35 +1,35 @@
 import React from 'react'
 import { createClient } from "contentful";
 import Image from 'next/image';
-const client = createClient({
-    space: process.env.SPACE_ID,
-    accessToken: process.env.ACCESS_TOKEN,
-});
+// const client = createClient({
+//     space: process.env.SPACE_ID,
+//     accessToken: process.env.ACCESS_TOKEN,
+// });
 
-const page = async (prop) => {
+const page =  (prop) => {
     const {params}=prop
     const {slug}=params
     
-    const fetchBlogPost = async (x) => {
-        const queryOptions = {
-          content_type: "blog",
-          "fields.slug[match]": x,
-        };
-        const queryResult = await client.getEntries(queryOptions);
-        return queryResult.items[0];
-      };
+    // const fetchBlogPost = async (x) => {
+    //     const queryOptions = {
+    //       content_type: "blog",
+    //       "fields.slug[match]": x,
+    //     };
+    //     const queryResult = await client.getEntries(queryOptions);
+    //     return queryResult.items[0];
+    //   };
 
-      const blogPost= await fetchBlogPost(slug)
-      const {title,content,image}=blogPost.fields
+    //   const blogPost= await fetchBlogPost(slug)
+    //   const {title,content,image}=blogPost.fields
 
   return (
     <div>
-      <Image src={`https:${image.fields.file.url}`} width={500} height={500} alt={title} />
+      {/* <Image src={`https:${image.fields.file.url}`} width={500} height={500} alt={title} />
         <div className="text-4xl text-center">{title}</div>
           
-         <p>{content}</p>
+         <p>{content}</p> */}
            
-        
+        xxx
             
            
       
@@ -39,13 +39,13 @@ const page = async (prop) => {
 
 export default page
 
-export async function generateStaticParams() {
-  const queryOptions = {
-    content_type: "blog",
-    select: "fields.slug",
-  };
-  const articles = await client.getEntries(queryOptions)
-  return articles.items.map((article) => ({
-    slug: article.fields.slug,
-  }));
-}
+// export async function generateStaticParams() {
+//   const queryOptions = {
+//     content_type: "blog",
+//     select: "fields.slug",
+//   };
+//   const articles = await client.getEntries(queryOptions)
+//   return articles.items.map((article) => ({
+//     slug: article.fields.slug,
+//   }));
+// }
--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
 import { createClient } from "contentful";
 import Link from 'next/link';
-import Image from 'next/image';
-const client =  createClient({
-  space: process.env.SPACE_ID,
-  accessToken: process.env.ACCESS_TOKEN,
-});
-const getBlogEntries = async () => {
-    const entries = await client.getEntries({ content_type: "blog" });
-    return entries;
-  };
-const page = async () => {
-    const blogEntries = await getBlogEntries();
+
+// const client =  createClient({
+//   space: process.env.SPACE_ID,
+//   accessToken: process.env.ACCESS_TOKEN,
+// });
+// const getBlogEntries = async () => {
+//     const entries = await client.getEntries({ content_type: "blog" });
+//     return entries;
+//   };
+const page =  () => {
+    // const blogEntries = await getBlogEntries();
     
   return (
     <div className='w-screen flex justify-center items-center gap-12 flex-col relative'>
         <div className="text-center text-5xl">Blog</div>
         <div className=" flex flex-col gap-8 w-[50%]">
-        {blogEntries.items.map((post)=>{
+        {/* {blogEntries.items.map((post)=>{
            
             const {title,slug,image,content} =post.fields
             
@@ -29,7 +29,7 @@ const page = async () => {
                
             </div>
            )
-        })}
+        })} */}
         </div>
      
     </div>
