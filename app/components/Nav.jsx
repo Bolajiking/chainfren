b7441c9c097237419eab67362cdb7b38ca5c81d9
Zion livingstone <zionlivingstone4@gmail.com>
2023-10-16 23:04:30 +0100

    x

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+'use client'
+import React, { useEffect,useState } from 'react'
 import Image from 'next/image'
 import logo from '../../public/logo.svg'
 const Nav = () => {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+  useEffect(()=>{
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      const visible = prevScrollPos > currentScrollPos;
+      setPrevScrollPos(currentScrollPos);
+      setVisible(visible);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  },[prevScrollPos])
   return (
-    <div className="bg-[#09011bce] text-white sticky top-0 z-20 backdrop-blur-xl ">
+    <div className={`bg-[#09011bce]  ${visible?'top-0':'top-[-100%]'} text-white sticky transition-[top] duration-[700ms] ease-in-out  z-20 backdrop-blur-xl `}>
     <div className='flex justify-between items-center xl:max-w-[1150px] sm:px-8 py-4    mx-auto'>
         <div className=""><Image src={logo} /></div>
         <div className="hidden items-center gap-9 md:flex font-normal">
