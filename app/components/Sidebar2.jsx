684559551f53e3d26a1eb3e3c7f012925d79ddf3
Zion livingstone <zionlivingstone4@gmail.com>
2023-11-01 16:22:21 +0100

    x

--- app/components/Sidebar2.jsx
+++ app/components/Sidebar2.jsx
@@ -12,8 +12,8 @@ const Sidebar2 = () => {
     <>
     <div className=" h-full flex-col mb-16 md:flex px-6  pt-8 hidden  top-0 left-0 w-full dark:text-[#6B6776] text-[#000000b9] dark:bg-primary bg-white
  gap-4 justify-start font-medium">
-        <div className="dark:text-white text-black text-lg">Web 3 for Beginners</div>
-        <div className="flex flex-col gap-4">
+        <div className="dark:text-white text-black text-lg">Introduction</div>
+        <div className="flex flex-col gap-4 overflow-auto">
           {heading?.map((h4)=>{
             return <div className="">{h4}</div>
           })}
@@ -28,7 +28,7 @@ const Sidebar2 = () => {
 {
           <div className={`fixed top-20  h-screen ${nav?'left-0':'left-[-100%]'} transition-all duration-300 w-[75%] dark:bg-primary bg-white flex flex-col gap-4 md:hidden px-6 pt-10 dark:text-[#ffffff3c] font-serif text-[#606060]
           `}>
-        <div className="dark:text-white text-black text-lg">Web 3 for Beginners</div>
+        <div className="dark:text-white text-black text-lg">Introduction</div>
         <div className="flex flex-col gap-4">
         {heading?.map((h4)=>{
             return <div className="">{h4}</div>
