501187c04f9faea3e90dd2b84810f0f8e7d9280f
Bolajiking <55434759+Bolajiking@users.noreply.github.com>
2024-07-21 13:07:58 +0100

    Update About.jsx

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
     <div id='about' className=' py-4  relative flex flex-col text-white justify-center items-center test bg-primary w-full 2xl:max-w-[1170px] mx-auto'>
         <div className="flex py-8 pb-0  flex-col items-center justify-center md:w-[700px]  lg:w-[1100px] px-5 md:px-0 text-center gap-8 lg:gap-0 z-[1]">
             <button className='bg-[#0079D8] px-4 py-1 rounded-2xl'>About Chainfren</button>
-            <div className="flex w-full gap-32 justify-center items-center">
+            <div className="flex w-full gap-y-36 justify-center items-center">
             <Image src={pic2} className='hidden lg:block' width={150} height={150} />
             <div className=" text-xl text-left my-2 ">Web3 provides society with a  last chance for building a fair, open and decentralized internet where economic value is widespread and not in the hands of a few. 
 <div className="">At Chainfren, Our mission is to onboard new stakeholders to harness the limitless potential of web3 and blockchain technologies and thrive in the evolving digital age.
