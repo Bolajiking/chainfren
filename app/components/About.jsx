79467713e005c5e1a87f2bbeda133a87344aca10
Bolajiking <55434759+Bolajiking@users.noreply.github.com>
2024-07-21 12:36:36 +0100

    Update About.jsx

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -12,8 +12,8 @@ const About = () => {
             <div className="flex w-full gap-32 justify-center items-center">
             <Image src={pic2} className='hidden lg:block' width={150} height={150} />
             <div className=" text-xl">Web3 provides society with a  last chance for building a fair, open and decentralized internet where economic value is widespread and not in the hands of a few. 
-At Chainfren, Our mission is to onboard new stakeholders to harness the limitless potential of web3 and blockchain technologies and thrive in the evolving digital age.
-Our team of industry experts is passionate about helping forward thinking creators, brands, and businesses who want to lead the charge in this new paradigm.</div>
+<div className="">At Chainfren, Our mission is to onboard new stakeholders to harness the limitless potential of web3 and blockchain technologies and thrive in the evolving digital age.
+Our team of industry experts is passionate about helping forward thinking creators, brands, and businesses who want to lead the charge in this new paradigm.</div></div>
             <Image src={pic3} className='hidden lg:block' width={200} height={200} />
         </div>
         <Image src={pic1} className='z-[1] px-4 md2:px-0' width={700} height={600} />
