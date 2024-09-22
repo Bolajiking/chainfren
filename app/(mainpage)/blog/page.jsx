38e887597f3a5a49a1fe590e41ffc7e0a299c9e7
Bolajiking <55434759+Bolajiking@users.noreply.github.com>
2024-09-22 09:19:49 +0100

    Update page.jsx

--- app/(mainpage)/blog/page.jsx
+++ app/(mainpage)/blog/page.jsx
@@ -39,8 +39,8 @@ const page = async  () => {
 {/*                             <div className="dark:text-[#FFFFFF99] text-[#606060]">Read More</div> */}
                             <div className="dark:text-[#FFFFFF99] text-[#606060]  text-sm"><Date datestring={date} /></div>
                         </div>
-                            <div className="text-2xl lg:text-3xl mt-3 font-bold font-medium font-serif ">{title}</div>
-                        <div className="dark:text-[#FFFFFF99] text-[#606060] text-base mt-3 ">{excerpt}</div>
+                            <div className="text-lg md:text-2xl lg:text-3xl mt-3 font-bold font-medium font-serif ">{title}</div>
+                        <div className="dark:text-[#FFFFFF99] text-[#606060] text-sm md:text-base lg:mt-3 ">{excerpt}</div>
                         
                         </div>
                         
