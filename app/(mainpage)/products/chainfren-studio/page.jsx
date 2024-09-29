84dcb268c047e2c8e6154e2cf2907627f8b3c2b3
bashirvdx <bjaji@vendelux.com>
2024-09-28 20:36:51 -0500

    Add links for pages

---
+++ app/(mainpage)/products/chainfren-studio/page.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const chainfrenStudio = () => {
+  return (
+    <div>
+    <div className="md:text-2xlClamp text-xlClamp px-5 md:px-0 sm:leading-[5.5rem] text-center font-bold mx-auto font-sans">
+<div className="flex md:w-[800px] lg:w-[850px] xl:w-[950px] flex-wrap justify-center gap-3">
+  <span className='sm:leading-[3.5rem] leading-[2.8rem]'>Unlocking</span>
+</div>
+<div className="bg-gradient-to-r from-white to-[#40ACFF] bg-clip-text text-transparent">
+  Digital Wealth
+</div>
+<div className="flex md:w-[800px] lg:w-[850px] xl:w-[950px] flex-wrap justify-center gap-3 mt-2">
+  <span className='sm:leading-[3.5rem] leading-[2.8rem]'>for</span>
+  <span className='sm:leading-[3.5rem] leading-[2.8rem]'>Creators</span>
+  <span className='sm:leading-[3.5rem] leading-[2.8rem]'>and</span>
+  <span className='sm:leading-[3.5rem] leading-[2.8rem]'>Brands</span>
+</div>
+</div>
+</div>
+  )
+}
+
+export default chainfrenStudio
